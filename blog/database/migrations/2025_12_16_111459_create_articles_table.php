61a7e3a76594033e052698674eac2e50eaeccade
Mehdy <mehdibentaleb548@gmail.com>
2025-12-30 11:39:23 +0100

    update in migrartion

--- blog/database/migrations/2025_12_16_111459_create_articles_table.php
+++ blog/database/migrations/2025_12_16_111459_create_articles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('slug')->unique();
-            $table->text('content');
+            $table->longText('content');
             $table->string('image')->nullable();
             $table->enum('status', ['draft', 'published', 'archived'])->default('draft');
             $table->integer('view_count')->default(0);
